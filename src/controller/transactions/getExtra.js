b03213bb8b1c4958a26695aded2ac18a84727f4b
asnorferreira <asnorferreira@gmail.com>
2024-02-01 09:43:30 -0300

    end extra

--- src/controller/transactions/getExtra.js
+++ src/controller/transactions/getExtra.js
@@ -1,36 +1,28 @@
-import pool from '../../configs/conection.js'
+import pool from "../../configs/conection.js";
 
 export const getExtra = async (req, res) => {
     const userId = req.usuario.id;
     const { filtro } = req.query;
 
     try {
-        let query = `
-            SELECT
-                t.id,
-                t.tipo,
-                t.descricao,
-                t.valor,
-                t.data,
-                t.usuario_id,
-                t.categoria_id,
-                c.descricao as categoria_nome
-            FROM
-                transacoes t
-            INNER JOIN
-                categorias c ON t.categoria_id = c.id
-            WHERE
-                t.usuario_id = $1
-        `;
-        let params = [userId];
+        let query = "SELECT * FROM transacoes WHERE usuario_id = $1";
+        const values = [userId];
 
-        if (filtro && Array.isArray(filtro) && filtro.length > 0) {
-            const placeholders = filtro.map((_, index) => `$${index + 2}`).join(', ');
-            query += ` AND c.descricao IN (${placeholders})`;
-            params = params.concat(filtro);
+        if (filtro) {
+            const filterArray = Array.isArray(filtro) ? filtro : [filtro];
+            query = `
+      SELECT * FROM transacoes 
+      WHERE 
+      usuario_id = $1 AND categoria_id 
+      IN (SELECT id FROM categorias 
+        WHERE 
+        descricao = ANY($2::text[]))`;
+            values.push(filterArray);
         }
-        const { rows } = await pool.query(query, params);
-        return res.status(200).json(rows);
+
+        const { rows } = await pool.query(query, values);
+
+        res.status(200).json(rows);
     } catch (error) {
         console.error("Erro ao listar transações: ", error);
         res.status(500).json({ mensagem: error.message });
